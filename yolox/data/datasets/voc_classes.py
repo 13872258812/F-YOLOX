28d3271f5052a3d4f85fc2cd5d1c97e8fc12309c
LF <2057253340@qq.com>
2024-02-17 11:31:27 +0800

    first commit

---
+++ yolox/data/datasets/voc_classes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# VOC_CLASSES = ( '__background__', # always index 0
+# VOC_CLASSES = (
+#     "aeroplane",
+#     "bicycle",
+#     "bird",
+#     "boat",
+#     "bottle",
+#     "bus",
+#     "car",
+#     "cat",
+#     "chair",
+#     "cow",
+#     "diningtable",
+#     "dog",
+#     "horse",
+#     "motorbike",
+#     "person",
+#     "pottedplant",
+#     "sheep",
+#     "sofa",
+#     "train",
+#     "tvmonitor",
+# )
+
+VOC_CLASSES = (
+    'bskl','large_bskl','lssp_cucumber','nfsq','large_lssp_cucumber',
+           'large_nfsq','large_yb','xb_bottle','xb_can','yb',
+)
